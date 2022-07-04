823c825845a24d0c232120a5988d312cb1199ff9
Luca Longo <l.longo@ambita.it>
2022-07-04 15:30:14 +0200

    wip

--- src/Models/Concerns/HasShippingAddresses.php
+++ src/Models/Concerns/HasShippingAddresses.php
@@ -2,20 +2,22 @@
 
 namespace Masterix21\Addressable\Models\Concerns;
 
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
 use Masterix21\Addressable\Concerns\UsesAddressableConfig;
 
 trait HasShippingAddresses
 {
     use UsesAddressableConfig;
 
-    public function shippingAddress()
+    public function shippingAddress(): MorphOne
     {
         return $this->morphOne($this->addressModel(), 'addressable')
             ->where('is_primary', true)
             ->where('is_shipping', true);
     }
 
-    public function shippingAddresses()
+    public function shippingAddresses(): MorphMany
     {
         return $this->morphMany($this->addressModel(), 'addressable')
             ->where('is_shipping', true);
