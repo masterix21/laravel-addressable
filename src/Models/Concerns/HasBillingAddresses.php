2d6faab2aa435b83a16e5f1471864112435ca654
Luca Longo <l.longo@ambita.it>
2022-07-04 15:29:39 +0200

    wip

--- src/Models/Concerns/HasBillingAddresses.php
+++ src/Models/Concerns/HasBillingAddresses.php
@@ -2,20 +2,22 @@
 
 namespace Masterix21\Addressable\Models\Concerns;
 
+use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
 use Masterix21\Addressable\Concerns\UsesAddressableConfig;
 
 trait HasBillingAddresses
 {
     use UsesAddressableConfig;
 
-    public function billingAddress()
+    public function billingAddress(): MorphOne
     {
         return $this->morphOne($this->addressModel(), 'addressable')
             ->where('is_primary', true)
             ->where('is_billing', true);
     }
 
-    public function billingAddresses()
+    public function billingAddresses(): MorphMany
     {
         return $this->morphMany($this->addressModel(), 'addressable')
             ->where('is_billing', true);
